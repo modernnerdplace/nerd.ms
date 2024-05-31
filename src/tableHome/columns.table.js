118085054f316108761bf3dcffcddaafc0e4f17c
vdBurgIT <fabio@vdburg.it>
2024-05-31 16:57:17 +0200

    fix

--- src/tableHome/columns.table.js
+++ src/tableHome/columns.table.js
@@ -75,7 +75,7 @@ export const columns = [
     className: "commands-data-table right",
     Cell: ({ cell: { value }, row: { original } }) => (
       <a href={`https://${value}.cmd.ms`} target="blank" rel="noreferrer noopener">
-        <b>{value}</b>.nerd.ms
+        <b>{value}</b>.cmd.ms
       </a>
     ),
   },
