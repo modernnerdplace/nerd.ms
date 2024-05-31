bad2d64cae2edd9fd5a1d547e432bf37ff942e41
Ferdi <ferdivt@delta-n.nl>
2024-05-31 15:10:13 +0200

    Converted commands variable to json file Created build task to create commands json from csv file

---
+++ tasks/createCommands/build.ts
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const csv = require("csvtojson")
+
+const csvFilePath = "config/commands.csv";
+const jsonFilePath = "static/commands.json";
+
+// Convert a csv file with csvtojson
+csv()
+  .fromFile(csvFilePath)
+  .then(function(jsonArrayObj){ //when parse finished, result will be emitted here.
+    fs.writeFile(jsonFilePath, JSON.stringify(jsonArrayObj), err => {
+        if (err) {
+          console.error(err);
+        } 
+      });
+   })
